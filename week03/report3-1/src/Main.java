570818fa1fa5b32efebe3f061e00a517a0b4ac82
2201207 <idksm31@kbu.ac.kr>
2025-12-07 21:46:34 +0900

    13주차

---
+++ week03/report3-1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("첫번째 숫자를 입력하세요 ");
+        int a = keyboard.nextInt();
+
+        System.out.print("두번째 숫자를 입력하세요 ");
+        int b = keyboard.nextInt();
+        int sum = a + b;
+        System.out.printf("%d + %d = %d", a, b, sum);
+    }
+    }
